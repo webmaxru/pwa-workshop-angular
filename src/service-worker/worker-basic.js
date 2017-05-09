846f00cd6f132327bb3e28bd91001874263c0bd9
Maxim Salnikov <salnikov@gmail.com>
2017-05-09 00:29:49 -0500

    sw bootstrap updated

--- src/service-worker/worker-basic.js
+++ src/service-worker/worker-basic.js
@@ -1,14 +1,20 @@
-import {StaticContentCache} from '@angular/service-worker/plugins/static';
-import {RouteRedirection} from '@angular/service-worker/plugins/routes';
-import {Push} from '@angular/service-worker/plugins/push';
 import {bootstrapServiceWorker} from '@angular/service-worker/worker';
+import {Dynamic, FreshnessStrategy, PerformanceStrategy} from '@angular/service-worker/plugins/dynamic';
+import {ExternalContentCache} from '@angular/service-worker/plugins/external';
+import {RouteRedirection} from '@angular/service-worker/plugins/routes';
+import {StaticContentCache} from '@angular/service-worker/plugins/static';
+import {Push} from '@angular/service-worker/plugins/push';
 import {CustomListeners} from './plugins/custom-listeners';
-
 
 bootstrapServiceWorker({
   manifestUrl: 'ngsw-manifest.json',
   plugins: [
     StaticContentCache(),
+    Dynamic([
+      new FreshnessStrategy(),
+      new PerformanceStrategy(),
+    ]),
+    ExternalContentCache(),
     RouteRedirection(),
     Push(),
     CustomListeners()
